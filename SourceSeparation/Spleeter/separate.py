4b5a0288b655631bdd8eaa1ec42119e0704aae29
Tiago António <aluno20000@ipt.pt>
2021-01-06 19:26:18 +0000

    Connection between Spleeter and Segmentation.js using RabbitMQ

--- SourceSeparation/Spleeter/separate.py
+++ SourceSeparation/Spleeter/separate.py
@@ -2,6 +2,13 @@ from spleeter.separator import Separator
 
 from time import perf_counter #from https://www.geeksforgeeks.org/time-perf_counter-function-in-python/
 
+import pika
+
+connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+channel = connection.channel()
+
+channel.queue_declare(queue='segmentation')
+
 # Using embedded configuration.
 separator = Separator('spleeter:2stems')
 # separator = Separator('spleeter:4stems')
@@ -19,6 +26,11 @@ t1_stop = perf_counter()
   
   
 print("Elapsed time in seconds:", t1_stop-t1_start) 
+
+channel.basic_publish(exchange='',
+                      routing_key='segmentation',
+                      body=audio[:-4])
+print(" [x] Sent " + audio)
 
 
 # spleeter separate -i song.wav -o ./Output -p spleeter:2stems
--- VidExtractor/segmentation.js
+++ VidExtractor/segmentation.js
@@ -1,60 +1,60 @@
 var ffmpeg = require('fluent-ffmpeg');
+var amqp = require('amqplib/callback_api');
 
-// var audio = 'ALZHF5UqnU4.wav';
-var duration; // file duration in seconds
-var nFiles; //number of files needed
-var inputSec=0;
+segmentation = async (path,audio) => {
+    // file metadata
+    ffmpeg.ffprobe(path + audio + '.wav', function (err, metadata) {
+        var inputSec = 0;
+        var duration = metadata.format.duration;
+        // Round a number upward to its nearest integer:
+        var nFiles = Math.ceil(duration / 15);
+        // console.log(nFiles)
+        for (let i = 1; i <= nFiles; i++) {
+            var command = ffmpeg(path + audio + '.wav')
+                .seekInput(inputSec)
+                .duration(30)
+                .withAudioChannels(1)
+                .withAudioFrequency(22500)
+                .on('error', function (err) {
+                    console.log('An error occurred: ' + err.message);
+                })
+                .on('end', function () {
+                    // console.log('Processing finished!')
+                })
+                .saveToFile(path + audio + i + ".wav");
 
-//using source separation with 2 stems (vocals and accompaniment) 
-var path = "/vagrant/SourceSeparation/Spleeter/Output/song/"
+            inputSec = inputSec + 15;
+        }
+    });
+}
 
-// file metadata
-ffmpeg.ffprobe(path + "vocals.wav", function (err, metadata) {
-    duration = metadata.format.duration;
-    // Round a number upward to its nearest integer:
-    nFiles = Math.ceil(duration / 15);
-    // console.log(nFiles)
-    for (let i = 1; i <= nFiles; i++) {
-        var command = ffmpeg(path + "vocals.wav")
-        .seekInput(inputSec)
-        .duration(30)
-        .withAudioChannels(1)
-        .withAudioFrequency(22500)
-        .on('error', function(err) {
-            console.log('An error occurred: ' + err.message);
-        })
-        .on('end', function () {
-            // console.log('Processing finished!')
-        })
-        .saveToFile(path + "vocals_" + i + ".wav");
 
-        inputSec = inputSec + 15;
+
+amqp.connect('amqp://localhost', function (error0, connection) {
+    if (error0) {
+        throw error0;
     }
+    connection.createChannel(function (error1, channel) {
+        if (error1) {
+            throw error1;
+        }
+        var queue = 'segmentation';
+
+        channel.assertQueue(queue, {
+            durable: false
+        });
+
+        console.log(" [*] Waiting for messages in %s. To exit press CTRL+C", queue);
+        channel.consume(queue, function (msg) {
+            var folder = msg.content.toString()
+            console.log(" [x] Received %s", folder);
+            //using source separation with 2 stems (vocals and accompaniment) 
+            var path = "/vagrant/SourceSeparation/Spleeter/Output/" + folder + "/"
+            segmentation(path,'vocals');
+            segmentation(path,'accompaniment');
+            console.log("  Finished")
+        }, {
+            noAck: true
+        });
+    });
 });
-
-// file metadata
-ffmpeg.ffprobe(path + "accompaniment.wav", function (err, metadata) {
-    duration = metadata.format.duration;
-    // Round a number upward to its nearest integer:
-    nFiles = Math.ceil(duration / 15);
-    // console.log(nFiles)
-    for (let i = 1; i <= nFiles; i++) {
-        var command = ffmpeg(path + "accompaniment.wav")
-        .seekInput(inputSec)
-        .duration(30)
-        .withAudioChannels(1)
-        .withAudioFrequency(22500)
-        .on('error', function(err) {
-            console.log('An error occurred: ' + err.message);
-        })
-        .on('end', function () {
-            // console.log('Processing finished!')
-        })
-        .saveToFile(path + "accompaniment_" + i + ".wav");
-
-        inputSec = inputSec + 15;
-    }
-});
-
-
-
